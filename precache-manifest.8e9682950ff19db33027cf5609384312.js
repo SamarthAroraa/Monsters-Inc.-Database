e90039e5da7f61cbb2df738e79db18407701a773
Samarth Arora <samartharora@Samarths-MacBook-Air.local>
2020-05-27 04:17:34 +0530

    Updates

---
+++ precache-manifest.8e9682950ff19db33027cf5609384312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70432d729a003049af333bb12a7ae29a",
+    "url": "/Monsters-Inc.-Database/index.html"
+  },
+  {
+    "revision": "f1e11321d53b488225c5",
+    "url": "/Monsters-Inc.-Database/static/css/main.57deb8af.chunk.css"
+  },
+  {
+    "revision": "90427e9d1794defa3333",
+    "url": "/Monsters-Inc.-Database/static/js/2.d679a014.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Inc.-Database/static/js/2.d679a014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e11321d53b488225c5",
+    "url": "/Monsters-Inc.-Database/static/js/main.a48fdc91.chunk.js"
+  },
+  {
+    "revision": "9bad12a33f88acd5d744",
+    "url": "/Monsters-Inc.-Database/static/js/runtime-main.c40d16ac.js"
+  }
+]);
